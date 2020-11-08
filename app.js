ed713b54b9a3fa92b47025352a57364ec4b064d8
Shubham Jain <kumarshubham1008@gmail.com>
2020-11-08 19:18:15 +0530

    inital route

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/movies', movieRouter);
 // app.use('/api/v1/tv', tvRouter);
-// app.use('/api/v1/anime', animeRouter);
-// app.use('/api/v1/manga', mangaRouter);
+app.use('/api/v1/anime', animeRouter);
+app.use('/api/v1/manga', mangaRouter);
 
 module.exports = app;
