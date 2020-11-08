6e5137b86043595147f5cf9abe4eb1d5a8930ca1
Shubham Jain <kumarshubham1008@gmail.com>
2020-11-08 19:26:41 +0530

    inital route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 // }
 
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
