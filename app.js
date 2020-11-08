03e348cc54992ff793b4744c97c872afa5318d81
Shubham Jain <kumarshubham1008@gmail.com>
2020-11-08 19:13:59 +0530

    inital route

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const movieRouter = require('./routes/movieRoutes');
-const tvRouter = require('./routes/tvRoutes');
-const animeRouter = require('./routes/animeRoutes');
-const mangaRouter = require('./routes/mangaRoutes');
+// const movieRouter = require('./routes/movieRoutes');
+// const tvRouter = require('./routes/tvRoutes');
+// const animeRouter = require('./routes/animeRoutes');
+// const mangaRouter = require('./routes/mangaRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+// }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
@@ -27,9 +27,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/movies', movieRouter);
-app.use('/api/v1/tv', tvRouter);
-app.use('/api/v1/anime', animeRouter);
-app.use('/api/v1/manga', mangaRouter);
+// app.use('/api/v1/movies', movieRouter);
+// app.use('/api/v1/tv', tvRouter);
+// app.use('/api/v1/anime', animeRouter);
+// app.use('/api/v1/manga', mangaRouter);
 
 module.exports = app;
