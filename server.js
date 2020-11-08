eba104d40c603b6ae2aa3efecc104ed89bf539e2
Shubham Jain <kumarshubham1008@gmail.com>
2020-11-08 18:35:52 +0530

    inital route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
+// const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 // const DB = process.env.DATABASE.replace(
 //   '<PASSWORD>',
 //   process.env.DATABASE_PASSWORD
